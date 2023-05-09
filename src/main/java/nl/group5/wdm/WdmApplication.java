8d25e8851745a57c4beb09695fe7bdd30a12e754
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2023-05-10 00:50:32 +0200

    spring init

---
+++ src/main/java/nl/group5/wdm/WdmApplication.java
@@ -0,0 +1,13 @@
+package nl.group5.wdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/group5/wdm/WdmApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.group5.wdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
